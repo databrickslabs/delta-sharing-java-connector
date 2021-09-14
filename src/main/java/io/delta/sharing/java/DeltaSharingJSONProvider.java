1f4bc75c60463f498c05bb9791c13c676a97f638
milos.colic <milos.colic@databricks.com>
2021-09-14 23:08:42 +0100

    Base code for fetching files to local.

---
+++ src/main/java/io/delta/sharing/java/DeltaSharingJSONProvider.java
@@ -0,0 +1,48 @@
+package io.delta.sharing.java;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.delta.sharing.spark.DeltaSharingProfile;
+import io.delta.sharing.spark.DeltaSharingProfileProvider;
+
+/**
+ * Load [[DeltaSharingProfile]] from a JSON string.
+ */
+public class DeltaSharingJSONProvider implements DeltaSharingProfileProvider {
+    String configuration;
+    DeltaSharingProfile profile;
+
+    public DeltaSharingJSONProvider(String conf) {
+        try {
+            configuration = conf;
+            ObjectMapper mapper = new ObjectMapper();
+            DeltaSharingProfileAdaptor profileAdaptor = mapper.readValue(configuration, DeltaSharingProfileAdaptor.class);
+            profile = profileAdaptor.toProfile();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+        if (profile.shareCredentialsVersion().isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Cannot find the 'shareCredentialsVersion' field in the profile file");
+        }
+
+        if ((int) profile.shareCredentialsVersion().get() > DeltaSharingProfile.CURRENT()) {
+            throw new IllegalArgumentException(
+                    "'shareCredentialsVersion' in the profile is " +
+                    "${profile.shareCredentialsVersion.get} which is too new. The current release " +
+                    "supports version ${DeltaSharingProfile.CURRENT} and below. Please upgrade to a newer " +
+                    "release.");
+        }
+        if (profile.endpoint() == null) {
+            throw new IllegalArgumentException("Cannot find the 'endpoint' field in the profile file");
+        }
+        if (profile.bearerToken() == null) {
+            throw new IllegalArgumentException("Cannot find the 'bearerToken' field in the profile file");
+        }
+    }
+
+
+    @Override
+    public DeltaSharingProfile getProfile() {
+        return profile;
+    }
+}
---
+++ src/main/java/io/delta/sharing/java/DeltaSharingProfileAdaptor.java
@@ -0,0 +1,43 @@
+package io.delta.sharing.java;
+
+import io.delta.sharing.spark.DeltaSharingProfile;
+import scala.Option;
+
+//Suppressed since getters are used only by Jackson via reflection
+@SuppressWarnings("unused")
+public class DeltaSharingProfileAdaptor {
+    int shareCredentialsVersion;
+    String endpoint;
+    String bearerToken;
+
+    public DeltaSharingProfileAdaptor() {
+    }
+
+    public int getShareCredentialsVersion() {
+        return shareCredentialsVersion;
+    }
+
+    public void setShareCredentialsVersion(int shareCredentialsVersion) {
+        this.shareCredentialsVersion = shareCredentialsVersion;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getBearerToken() {
+        return bearerToken;
+    }
+
+    public void setBearerToken(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+
+    public DeltaSharingProfile toProfile() {
+        return DeltaSharingProfile.apply(Option.apply(shareCredentialsVersion), endpoint, bearerToken);
+    }
+}
