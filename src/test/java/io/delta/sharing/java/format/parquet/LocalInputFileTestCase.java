e46d0e64e4a09870c28840e2f67fb3c121faa963
milos.colic <milos.colic@databricks.com>
2022-06-22 03:29:03 +0100

    Improve test coverage for LocalInputFile class.

--- src/test/java/io/delta/sharing/java/format/parquet/LocalInputFileTestCase.java
+++ src/test/java/io/delta/sharing/java/format/parquet/LocalInputFileTestCase.java
@@ -1,9 +1,5 @@
 package io.delta.sharing.java.format.parquet;
 
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.parquet.avro.AvroParquetReader;
-import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.io.SeekableInputStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,8 +8,6 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
 
 public class LocalInputFileTestCase {
 
@@ -39,6 +33,11 @@ public class LocalInputFileTestCase {
                 () -> stream.read(b, 100, 100),
                 "assert no exception for read with offset using a buffer"
         );
+
+        Assertions.assertDoesNotThrow(
+                () -> stream.readFully(b, 100, 100),
+                "assert no exception for read with offset using a buffer"
+        );
     }
 
     @Test
@@ -60,8 +59,8 @@ public class LocalInputFileTestCase {
         Path filePath = Paths.get("src", "test", "resources", "table_reader_test_data.parquet").toAbsolutePath();
         LocalInputFile localInputFile = new LocalInputFile(filePath);
 
-
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        //8192 is page size constant inside the LocalInputFile
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(8192 * 4);
         SeekableInputStream stream = localInputFile.newStream();
 
         Assertions.assertDoesNotThrow(
@@ -72,6 +71,28 @@ public class LocalInputFileTestCase {
         Assertions.assertDoesNotThrow(
                 () -> stream.readFully(byteBuffer),
                 "assert no exception for readFully with byte buffer"
+        );
+    }
+
+    @Test
+    public void testPointers() throws IOException {
+        Path filePath = Paths.get("src", "test", "resources", "table_reader_test_data.parquet").toAbsolutePath();
+        LocalInputFile localInputFile = new LocalInputFile(filePath);
+
+        //8192 is page size constant inside the LocalInputFile
+        SeekableInputStream stream = localInputFile.newStream();
+
+        Assertions.assertTrue(stream.markSupported());
+        Assertions.assertEquals(stream.available(), 0);
+
+        Assertions.assertDoesNotThrow(
+                () -> stream.mark(0),
+                "assert mark operation doesnt throw an exception"
+        );
+
+        Assertions.assertDoesNotThrow(
+                () -> stream.mark(0),
+                "assert mark operation doesnt throw an exception"
         );
     }
 }
