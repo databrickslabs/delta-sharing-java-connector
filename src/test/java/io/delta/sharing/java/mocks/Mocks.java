6d5dad97412becfdd6a59dff5713720ea7ab85a4
milos.colic <milos.colic@databricks.com>
2022-06-22 03:19:42 +0100

    Improve test coverage. Add tests for LocalInputFile class. Improve the tests for DeltaSharingJSONProvider.

--- src/test/java/io/delta/sharing/java/mocks/Mocks.java
+++ src/test/java/io/delta/sharing/java/mocks/Mocks.java
@@ -6,4 +6,20 @@ public class Mocks {
             "  \"endpoint\": \"https://sharing.delta.io/delta-sharing/\",\n" +
             "  \"bearerToken\": \"faaie590d541265bcab1f2de9813274bf233\"\n" +
             "}";
+
+    public static String ProviderJSONUnsupportedCredentials = "{\n" +
+            "  \"shareCredentialsVersion\": 12,\n" +
+            "  \"endpoint\": \"https://sharing.delta.io/delta-sharing/\",\n" +
+            "  \"bearerToken\": \"faaie590d541265bcab1f2de9813274bf233\"\n" +
+            "}";
+
+    public static String ProviderJSONNoEndpoint = "{\n" +
+            "  \"shareCredentialsVersion\": 12,\n" +
+            "  \"bearerToken\": \"faaie590d541265bcab1f2de9813274bf233\"\n" +
+            "}";
+
+    public static String ProviderJSONNoToken = "{\n" +
+            "  \"shareCredentialsVersion\": 1,\n" +
+            "  \"endpoint\": \"https://sharing.delta.io/delta-sharing/\",\n" +
+            "}";
 }
