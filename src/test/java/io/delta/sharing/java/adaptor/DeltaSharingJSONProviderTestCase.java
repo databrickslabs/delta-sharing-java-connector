4123475d7e97cb5e2d3a4a7dcd6890a9c0c14209
milos.colic <milos.colic@databricks.com>
2022-06-22 03:39:38 +0100

    Improve test coverage for DeltaSharingJSONProvider class.

--- src/test/java/io/delta/sharing/java/adaptor/DeltaSharingJSONProviderTestCase.java
+++ src/test/java/io/delta/sharing/java/adaptor/DeltaSharingJSONProviderTestCase.java
@@ -6,9 +6,12 @@ import io.delta.sharing.java.mocks.Mocks;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 
 public class DeltaSharingJSONProviderTestCase {
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
     void testInstanceCreation() throws JsonProcessingException {
         String providerJSON = Mocks.providerJSON;
@@ -21,6 +24,21 @@ public class DeltaSharingJSONProviderTestCase {
                         Assertions.assertEquals(provider.getProfile().endpoint(), "https://sharing.delta.io/delta-sharing/"),
                 () ->
                         Assertions.assertEquals(provider.getProfile().bearerToken(), "faaie590d541265bcab1f2de9813274bf233")
+        );
+
+        Assertions.assertDoesNotThrow(
+                () -> {
+                    DeltaSharingProfileAdaptor adaptor = new DeltaSharingProfileAdaptor();
+                    adaptor.setShareCredentialsVersion((int) provider.getProfile().shareCredentialsVersion().get());
+                    adaptor.setEndpoint(provider.getProfile().endpoint());
+                    adaptor.setBearerToken(provider.getProfile().bearerToken());
+                    adaptor.setExpirationTime(provider.getProfile().expirationTime());
+                    Objects.equals(adaptor.getShareCredentialsVersion(), provider.getProfile().shareCredentialsVersion().get());
+                    Objects.equals(adaptor.getEndpoint(), provider.getProfile().endpoint());
+                    Objects.equals(adaptor.getBearerToken(), provider.getProfile().bearerToken());
+                    Objects.equals(adaptor.getExpirationTime(), provider.getProfile().expirationTime());
+                },
+                "adaptors format should match the provider format."
         );
     }
 
