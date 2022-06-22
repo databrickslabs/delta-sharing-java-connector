8ec5bc163350662fbb7ceae8c989a6ddc1587c61
milos.colic <milos.colic@databricks.com>
2022-06-22 10:49:01 +0100

    Fix version number in java docs.

--- src/main/java/io/delta/sharing/java/DeltaSharing.java
+++ src/main/java/io/delta/sharing/java/DeltaSharing.java
@@ -39,7 +39,7 @@ import java.util.regex.Pattern;
  * <p>
  *
  * @author Milos Colic
- * @since 1.0.0
+ * @since 0.1.0
  */
 public class DeltaSharing {
     DeltaSharingProfileProvider profileProvider;
